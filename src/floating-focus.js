c68bf17bb4700996ba3684390dc6bca291050941
Jiri Swen <jiri.ei@hotmail.com>
2019-12-18 14:06:29 +0100

    Simplify object deep equality

--- src/floating-focus.js
+++ src/floating-focus.js
@@ -218,18 +218,8 @@ export default class FloatingFocus {
 		};
 	}
 
-	targetRectChanged(rect1, rect2) {
-		return !(
-			rect1.left === rect2.left &&
-			rect1.top === rect2.top &&
-			rect1.width === rect2.width &&
-			rect1.height === rect2.height
-		);
-	}
-
 	monitorElementPosition() {
-		if (!(this.target && this.previousTargetRect
-				&& this.targetRectChanged(this.previousTargetRect, this.target.getBoundingClientRect()))) {
+		if (!(this.target && this.previousTargetRect && !Object.is(this.previousTargetRect, this.target.getBoundingClientRect()))) {
 			return;
 		}
 
