c498b9640dd68cc369946436af9dda3b3a0a6e35
Jiri Swen <jiri.ei@hotmail.com>
2020-10-29 11:40:46 +0100

    Bring back CleanWebpackPlugin with exclude patterns

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,16 @@
 const merge = require('webpack-merge');
 const styled = require('./webpack.styled.js');
 const unstyled = require('./webpack.unstyled.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const prodConfig = {
+	mode: 'production',
+	plugins: [
+		new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['**/*', '!unstyled.js', '!floating-focus.js', '!floating-focus.css'] })
+	]
+};
 
 module.exports = [
-	merge(styled, {
-		mode: 'production'
-	}),
-	merge(unstyled, {
-		mode: 'production'
-	}),
+	merge(styled, prodConfig),
+	merge(unstyled, prodConfig),
 ];
